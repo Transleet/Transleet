cf29882d3fbd9e0854af25af4dde146c71ad9b76
Summpot <summpot@outlook.com>
2022-05-22 07:50:13 +0000

    Update

--- src/api/Controllers/AccountController.cs
+++ src/api/Controllers/AccountController.cs
@@ -1,6 +1,4 @@
-﻿#nullable enable
-
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using Transleet.Models;
